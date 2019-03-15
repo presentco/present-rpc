6bf5580bf0c5bea30fc633bcbd67921d6f8ae0c3
Patrick Niemeyer <pat@pat.net>
2019-03-14 21:50:50 -0500

    Adding SHA

--- java/compiler/homebrew/create-archive.sh
+++ java/compiler/homebrew/create-archive.sh
@@ -11,3 +11,9 @@ rm -f $archive
 # -j ignores paths and places the files at the top level
 zip -j $archive ../build/libs/present-rpc-compiler.jar scripts/present-rpc.sh
 
+# Report the sum
+echo "\n"
+echo "Update the published brew formula with the following SHA and archive name:"
+shasum -a 256 $archive
+
+
